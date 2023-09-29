880a0581e9ac1bbecfb051e020443d4b23e82dc7
nadeshda18 <nadeshda02@hotmail.com>
2023-09-29 11:49:27 -0400

    reverse

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-    print('{:02d}'.format(numbers), end="")
-    if numbers != 99:
-        print(', ', end="")
+for number in range(0, 100):
+    if number == 99:
+        print('{}'.format(number))
+        break
+    print("{:02d}, ".format(number), end="")
