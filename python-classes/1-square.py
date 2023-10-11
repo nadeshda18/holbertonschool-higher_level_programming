dc5b3940432bd981389d11ad35dc3ff505b59b64
nadeshda18 <nadeshda02@hotmail.com>
2023-10-11 19:53:46 -0400

    define a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+"""This module contains a class Square that defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size):
+        """This method initializes a new instance of Square"""
+        self.__size = size
