3fd39fd5df6f4f2c2ad11c153e5e528b3c154cc9
nadeshda18 <nadeshda02@hotmail.com>
2023-10-12 07:23:48 -0400

    changed comments

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""This module contains a class Square that defines a square
--Private instance attribute is size
+"""This module contains a class Square that defines a square:
+-Private instance attribute: size
 -Instantiation with size(size: def __init__(self, size=0)
 """
 
 
 class Square:
-    """This class defines a square"""
+    """This class defines a square
+
+    Attributes:
+        __size (int): size of the square
+    """
 
     def __init__(self, size=0):
         """This method initializes a new instance of Square
+
         Raises:
-            TypeError if not an integer
-            ValueError if size if <0
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >=0")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
