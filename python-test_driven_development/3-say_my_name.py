f42d1c78196df865948b90e25f2779b33a04dedb
nadeshda18 <nadeshda02@hotmail.com>
2023-10-13 14:05:39 -0400

    #3 project, say my name and test cases

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Function to print string "My name is <first name> <last name>"
+Prototype: def say_my_name(first_name, last_name="")
+first_name and last_name must be strings otherwise,
+-TypeError
+        ("first_name must be a string") or
+        ("last_name must be a string")
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    -TypeError
+            ("first_name must be a string") or
+            ("last_name must be a string")
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
