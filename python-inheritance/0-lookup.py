657365a60b778a0ef4ad4a20eb833dc82cc4cb51
nadeshda18 <nadeshda02@hotmail.com>
2023-10-19 00:40:13 -0400

    adjusted comments

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Function to return a list of available
+attributes and methods of an object"""
+
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
