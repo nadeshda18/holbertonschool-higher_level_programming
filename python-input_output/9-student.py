30dc1244e8ff509112bc0a387f4b2201a5eb4d7b
nadeshda18 <nadeshda02@hotmail.com>
2023-10-19 16:15:03 -0400

    added comments

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -12,11 +12,18 @@ Public method def to_json(self): that retrieves a dictionary representation
 
 
 class Student:
+    """creates a class Student"""
 
     def __init__(self, first_name, last_name, age):
+        """Attributes:
+        first_name
+        last_name
+        age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """retrieves a dictionary representation"""
         return self.__dict__
