53cc79592fe740e0d466a6fb3cf7b0304267c9bd
nadeshda18 <nadeshda02@hotmail.com>
2023-10-19 16:06:19 -0400

    return the dictionary

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a function that returns the dictionary description with
+simple data structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object:
+Prototype: def class_to_json(obj):
+obj is an instance of a Class
+All attributes of the obj Class are serializable:
+list, dictionary, string, integer and boolean
+"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
