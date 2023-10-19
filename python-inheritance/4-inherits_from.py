c1b4477d5ff6cf990eb2ad2ac279688abcc30145
nadeshda18 <nadeshda02@hotmail.com>
2023-10-19 01:13:37 -0400

    had "from" in the comments

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """
 Write a function that returns True if the object is an instance of a class
 that inherited (directly or indirectly)
-from the specified class ; otherwise False.
+the specified class ; otherwise False.
 
 Prototype: def inherits_from(obj, a_class):
 """
