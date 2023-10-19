8432e46f115811efc79af5d35b7b34781893118e
nadeshda18 <nadeshda02@hotmail.com>
2023-10-19 16:00:36 -0400

    add arguments to a list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,6 @@ and then save them to a file:
 You must use your function save_to_json_file from 5-save_to_json_file.py
 You must use your function load_from_json_file from 6-load_from_json_file.py
 The list must be saved as a JSON representation in a file named add_item.json
-If the file doesn’t exist, it should be created
 """
 
 
