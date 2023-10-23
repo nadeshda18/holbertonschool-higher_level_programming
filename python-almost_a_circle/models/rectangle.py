6e13d1ea1d2721387aed25bd049871985bd149b4
nadeshda18 <nadeshda02@hotmail.com>
2023-10-23 19:52:06 -0400

    added comments

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,8 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """class Rectangle that inherits from Base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Init Method
         Args:
