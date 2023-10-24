057bb1f50a2635b634ee8f7de3909a095c0e4334
nadeshda18 <nadeshda02@hotmail.com>
2023-10-24 03:24:23 -0400

    little changes

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Test module for testing Base Class"""
+"""unittest for testing Base Class"""
 
 
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
-import models.rectangle
-import models.square
-import models.base
 
 
 class TestBase(unittest.TestCase):
-    """Class for the test"""
+    """Class for the test Base"""
 
     def test_base(self):
         """tests"""
-        a = Base()
-        self.assertEqual(a.id, 1)
-        b = Base()
-        self.assertEqual(b.id, 2)
-        a = Base(89)
-        self.assertEqual(a.id, 89)
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(89)
+        self.assertEqual(b3.id, 89)
+
+
+if __name__ == "__main__":
+    unittest.main()
