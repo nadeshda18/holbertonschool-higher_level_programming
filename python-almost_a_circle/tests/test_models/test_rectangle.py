b057f0f8061ceb94c8ac44d23b648c74eb92db69
nadeshda18 <nadeshda02@hotmail.com>
2023-10-24 03:27:51 -0400

    test rectangle...test square

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""unittest for class Rectangle"""
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """class for the test Rectangle"""
+
+    def test_area(self):
+        """tests"""
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""unittest for class Square"""
+
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """class for the Test Square"""
+
+    def test_square(self):
+        """tests"""
+
+
+if __name__ == "__main__":
+    unittest.main()
