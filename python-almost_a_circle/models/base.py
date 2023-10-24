b3452a9851355a2d9fdcb28d783b749e6349b846
nadeshda18 <nadeshda02@hotmail.com>
2023-10-23 22:10:19 -0400

    dummy

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,15 @@ JSON string representation json_string:
     -json_string is a string representing a list of dictionaries
     -If json_string is None or empty, return an empty list
     -Otherwise, return the list represented by json_string
-
+Class method def create(cls, **dictionary): that returns an instance with all attributes already set:
+    **dictionary can be thought of as a double pointer to a dictionary
+    To use the update method to assign all attributes,
+    you must create a “dummy” instance before:
+Create a Rectangle or Square instance with “dummy” mandatory attributes
+    (width, height, size, etc.)
+Call update instance method to this “dummy” instance to apply your real values
+    You must use the method def update(self, *args, **kwargs)
+    **dictionary must be used as **kwargs of the method update
 """
 
 import json
@@ -81,3 +89,16 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set
+        Args:
+            -**dictionary
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
