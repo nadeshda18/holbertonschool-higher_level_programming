23ef0f08f888089d39076250e31069097ed0699d
nadeshda18 <nadeshda02@hotmail.com>
2023-10-23 21:57:53 -0400

    added static method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,8 +5,13 @@ Private class attribute: __nb_objects = 0
 Class constructor: def __init__(self, id=None)
     - if id is not None assign the public instance attribute id
     -otherwise increment __nb_objects and assign new value to id
-
+Static method def to_json_string(list_dictionaries):
+    -list_dictionaries is a list of dictionaries
+    -If list_dictionaries is None or empty, return the string: "[]"
+    -Otherwise, return the JSON string representation of list_dictionaries
 """
+
+import json
 
 
 class Base:
@@ -28,3 +33,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries
+        Args:
+            -list_dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
