251e49868c3b42aa55d5983df3fd9757a351251c
nadeshda18 <nadeshda02@hotmail.com>
2023-10-23 21:08:03 -0400

    *args and **kwargs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,6 +13,19 @@ The overloading __str__ method should return [Square] (<id>) <x>/<y> - <size> -
 in our case, width or height
 Setter should assign width and height with the same value
 Setter should have the same value validation as Rectangle for width and height
+Update the class Square by adding the public method
+def update(self, *args, **kwargs) that assigns attributes:
+    *args is the list of arguments - no-keyword arguments
+        1st argument should be the id attribute
+        2nd argument should be the size attribute
+        3rd argument should be the x attribute
+        4th argument should be the y attribute
+    **kwargs can be thought of as a double pointer to a dictionary:
+        key/value (keyword arguments)
+    **kwargs must be skipped if *args exists and is not empty
+Each key in this dictionary represents an attribute to the instance
+
+
 """
 
 from models.rectangle import Rectangle
@@ -51,3 +64,26 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """update method"""
+        if args is not None and len(args) > 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.size = args[i]
+                elif i == 2:
+                    self.x = args[i]
+                elif i == 3:
+                    self.y = args[i]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
