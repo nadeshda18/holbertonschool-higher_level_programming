605511bbed566bd96d3f3007369aa2c1c6ba4c06
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 18:50:16 -0400

    a little different

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,15 @@
 -- lists all cities contained database hbtn_0d_usa
+
 -- display: cities.id - cities.name - states.name
+
 -- results must be sorted in ascending order by cities.id
+
 -- You can only use one SELECT statement
-USE hbtn_0d_usa
-SELECT cities.id, cities.name, states.name AS state_name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+
+SELECT
+    cities.id,
+    cities.name,
+    states.name
+FROM cities
+    INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
