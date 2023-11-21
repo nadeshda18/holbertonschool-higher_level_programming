adcd88ad97186abb23439f525693eb15043254bc
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 19:31:06 -0400

    Dexter

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- lists all genres of the show DEXTER
+
+-- display tv_genres.name
+
+-- results must be sorted ascending order by genre name
+
+SELECT tv_genres.name
+FROM tv_genres
+    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
