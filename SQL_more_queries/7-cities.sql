0c00f1be19034e38ccc1610aeed1f37e6d414926
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 00:11:17 -0400

    create database, table, id, name, state id

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- create database hbtn_0d_usa and table cities
+-- id=INT, unique, auto-generated, can't be null and primary key
+-- state-id=INT, can't be null, must be foreign key that references to id of states table
+-- name=VARCHAR(256)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS `cities` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `state_id` INT NOT NULL, `name` VARCHAR(256) NOT NULL);
