74ee0e81dcbed0204e9c8f7dde3db75eb2615c59
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 15:56:00 -0400

    database dump

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- import database dump from hbtn_0d_tvshows
+-- lists all shows contained that have at least one genre linked
+-- display: tv_shows.title - tv_show_genres.genre_id
+-- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can only use one SELECT statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
