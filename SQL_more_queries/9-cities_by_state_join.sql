0eb8b3106ff629cbb48331eb6b7ff653008e3390
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 15:51:58 -0400

    and another try

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -3,4 +3,4 @@
 -- results must be sorted in ascending order by cities.id
 -- You can only use one SELECT statement
 USE hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name AS state_name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
