c876d84ac742b2f9bc0fb4f70f151be307e9694c
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 19:19:12 -0400

    forgot a s

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,10 +5,10 @@
 -- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
 SELECT
-    tv_show.title,
+    tv_shows.title,
     tv_show_genres.genre_id
 from tv_shows
-    LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY
     tv_shows.title ASC,
     tv_show_genres.genre_id ASC;
