7f8fadff5cc95a36c2a232f1f92a71bfcf3f3233
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 18:04:19 -0400

    - instead of _

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,19 @@
 -- lists all cities of california that can be found in database hbtn_0d_usa
+
 -- states table contains record where name=california
+
 -- results must be sorted in ascending order by cities.id
-USE hbtn-0d_usa;
 
-SET @california_id = (SELECT id FROM states WHERE name = 'California');
+USE hbtn_0d_usa;
 
-SELECT id, name FROM cities WHERE states_id = @california_id ORDER BY id ASC;
+SET @california_id = (
+        SELECT id
+        FROM states
+        WHERE name = 'California'
+    );
+
+SELECT id, name
+FROM cities
+WHERE
+    states_id = @california_id
+ORDER BY id ASC;
