daceb2037b5469f76daae5ca651465c1f6b65eca
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 00:02:57 -0400

    create database and table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- create database hbtn_0d_usa and table states
+-- id=INT unique, auto generated, can't be null and is primary key
+-- name=VARCHAR(256) can't be null
+CREATE TABLE IF NOT EXISTS `states` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR(256) NOT NULL);
