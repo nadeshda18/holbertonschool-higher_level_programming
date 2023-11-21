294e4e25e0bd723165822561b06965108dc3cd57
nadeshda18 <nadeshda02@hotmail.com>
2023-11-21 19:17:41 -0400

    left join

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,14 @@
+-- import database dump and lists all shows
+
+-- display: tv_show.title - tv_show_genres.genre_id
+
+-- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+
+SELECT
+    tv_show.title,
+    tv_show_genres.genre_id
+from tv_shows
+    LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
